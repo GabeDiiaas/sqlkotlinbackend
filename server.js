a538e59b61fb4d1271ba7289eab8d206c4ec2388
Gabriel Dias <gabrielgarfieldorg@gmail.com>
2024-12-26 19:08:40 -0300

    atualizacoes var.

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
+require('dotenv').config();  // Carrega as variáveis de ambiente do arquivo .env
 const express = require('express');
 const bodyParser = require('body-parser');
 const sql = require('mssql');
 const cors = require('cors');
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 // Middleware
 app.use(bodyParser.json());
 app.use(cors());
 
-// Configuração do banco de dados
+// Configuração do banco de dados com variáveis de ambiente
 const defaultDbConfig = {
-    user: 'usuario Kotlin',
-    password: '12345',
-    server: '192.168.1.16',  // IP padrão, será substituído se um IP for fornecido
-    database: 'Banco_teste',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    server: process.env.DB_SERVER,
+    database: process.env.DB_DATABASE,
     options: {
         encrypt: true,
         trustServerCertificate: true,
@@ -41,20 +42,21 @@ app.get('/products', async (req, res) => {
         // Consulta SQL com as tabelas e campos solicitados
         const query = `
             SELECT
-                M.MAT_DESC,
-                M.MAT_CODI,
-                M.MAT_REFE,
-                P.TAB_PREC0,
-                E.EST_QUAN,
-                I.IMAGEM
-            FROM
-                MATERIAL AS M
-            JOIN
-                PRECOVENDA AS P ON M.MAT_CODI = P.MAT_CODI
-            JOIN
-                ESTOQUE AS E ON M.MAT_CODI = E.MAT_CODI
-            LEFT JOIN
-                IMAGENS AS I ON M.MAT_CODI = I.MAT_CODI
+    M.MAT_DESC,
+    M.MAT_CODI,
+    M.MAT_REFE,
+    P.TAB_PREC0, 
+    E.EST_QUAN,
+    I.IMAGEM
+FROM
+    MATERIAL AS M
+JOIN
+    PRECOVENDA AS P ON M.MAT_CODI = P.MAT_CODI
+JOIN
+    ESTOQUE AS E ON M.MAT_CODI = E.MAT_CODI
+LEFT JOIN
+    IMAGENS AS I ON M.MAT_CODI = I.MAT_CODI
+
         `;
 
         let result = await pool.request().query(query);  // Executa a consulta SQL
